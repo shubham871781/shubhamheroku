14ed40a7fde96933941ae7a55c468118bca38a42
Sourabh Kumar Tomar <SourabhTomar1111@users.noreply.github.com>
2022-08-31 12:04:27 +0530

    first commit nhi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './Coompontent/Header/Header'
+import Sidebar from './Coompontent/Sidebar/Sidebar'
+import Dashboard from './Coompontent/Dashboard/Dashboard'
+import Form from './Coompontent/Form/Form'
+import Patient_details from "./Coompontent/Patient_details/Patient_details"
+import Patient from "./Coompontent/Patient_details/Patient"
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="wrapper">
+    {/* <div id="loader"></div> */}
+    <Header />
+    <Sidebar />
+    <Routes>
+      <Route path="/" element={<Dashboard />}></Route>
+      <Route path="/from" element={<Form />}></Route>
+      <Route path="/patient-details" element={<Patient_details />}></Route> 
+      <Route path="/patient" element={<Patient />}></Route> 
+    </Routes>
+  </div>
   );
 }
 
